d3a46d7873fcef72fa5d99a3e7a21e965ed135db
sougabriel <gabriel.dsouzapro@gmail.com>
2024-07-17 16:38:10 -0300

    chore: add project providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,26 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+    ApplicationConfig,
+    provideZoneChangeDetection,
+    isDevMode,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideServiceWorker } from '@angular/service-worker';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+    providers: [
+        provideZoneChangeDetection({ eventCoalescing: true }),
+        { provide: LocationStrategy, useClass: PathLocationStrategy },
+        provideRouter(routes),
+        provideAnimationsAsync(),
+        provideHttpClient(withFetch()),
+        provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000',
+        }),
+    ],
 };
