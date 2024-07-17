78d21b88cc92492647bfc88c032550db3403c7fd
sougabriel <gabriel.dsouzapro@gmail.com>
2024-07-17 16:49:23 -0300

    feat: create social media component

---
+++ src/app/shared/social/social.ts
@@ -0,0 +1,21 @@
+import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
+import { AsyncPipe, NgOptimizedImage } from '@angular/common';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { ContactService } from '../../api';
+import { ClipboardModule } from '@angular/cdk/clipboard'
+
+@Component({
+    selector: 'socials',
+    standalone: true,
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [NgOptimizedImage, MatTooltipModule, ClipboardModule, AsyncPipe],
+    templateUrl: './social.html',
+    styleUrl: './social.scss',
+})
+export class Social {
+    height = input<string>('64');
+    width = input<string>('64');
+
+    protected readonly contactService = inject(ContactService);
+    protected readonly contacts$ = this.contactService.getContacts();
+}
