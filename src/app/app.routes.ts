4524857ee72e1a57febea2aa74d16c58bb1ac812
sougabriel <gabriel.dsouzapro@gmail.com>
2024-07-18 15:14:06 -0300

    chore: create routes and lazy load them for better performance

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    {
+        path: 'home',
+        title: 'Homepage - Sou Gabriel',
+        loadComponent: () => import('./pages/homepage').then((m) => m.Homepage),
+    },
+    {
+        path: 'projects',
+        title: 'Projects',
+        loadChildren: () =>
+            import('./pages/projects/projects.routes').then((r) => r.routes),
+    },
+    {
+        path: '404',
+        title: 'Page Not Found',
+        loadComponent: () =>
+            import('./pages/not-found').then((m) => m.NotFound),
+    },
+    { path: '**', redirectTo: '/404' },
+];
