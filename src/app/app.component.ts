ca21b7b2424e02b2883dc49ba246afa08493e127
sougabriel <gabriel.dsouzapro@gmail.com>
2024-07-18 15:22:23 -0300

    feat: update app component imports and html template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CookiePopup } from './shared/cookie-popup/cookie-popup';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [RouterOutlet, NavbarComponent, CookiePopup],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'sougabriel-portfolio';
-}
+export class AppComponent {}
