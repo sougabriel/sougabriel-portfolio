3a6a884235a5ea1ad27a8e31e507143c84cf6862
sougabriel <gabriel.dsouzapro@gmail.com>
2024-07-18 15:17:55 -0300

    feat: create projects details component

---
+++ src/app/pages/projects/projects-list/project-details/project-details.ts
@@ -0,0 +1,34 @@
+import { ChangeDetectionStrategy, Component, inject } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MatDialogActions, MatDialogClose, MatDialogContent, MatDialogRef, MatDialogTitle, MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { Project } from "../../../../api";
+import { MatIconModule } from "@angular/material/icon";
+import { MatTooltip } from "@angular/material/tooltip";
+import { RouterLink } from "@angular/router";
+
+@Component({
+    selector: 'project-detail',
+    standalone: true,
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [
+        MatButtonModule,
+        MatDialogActions,
+        MatDialogClose,
+        MatDialogTitle,
+        MatDialogContent,
+        MatIconModule,
+        MatTooltip,
+        RouterLink,
+    ],
+    templateUrl: './project-details.html',
+    styleUrl: './project-details.scss',
+})
+export class ProjectDetails {
+    protected readonly dialogRef = inject(MatDialogRef<ProjectDetails>);
+    protected readonly project: Project = inject(MAT_DIALOG_DATA);
+
+    protected isDisable(value: string): boolean {
+        if (value == '') return true;
+        return false;
+    }
+}
