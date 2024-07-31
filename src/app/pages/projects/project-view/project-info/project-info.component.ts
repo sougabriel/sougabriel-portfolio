ea45849a698f9d3ef762211cea7e7a6088bc3355
sougabriel <gabriel.dsouzapro@gmail.com>
2024-07-31 12:22:54 -0300

    feat: create project info display component

---
+++ src/app/pages/projects/project-view/project-info/project-info.component.ts
@@ -0,0 +1,19 @@
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { Project } from '@api/interfaces';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatTooltip } from '@angular/material/tooltip';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-project-info',
+  standalone: true,
+  imports: [MatButtonModule, MatIcon, MatChipsModule, MatTooltip, RouterLink],
+  templateUrl: './project-info.component.html',
+  styleUrl: './project-info.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ProjectInfoComponent {
+    project = input.required<Project>();
+}
