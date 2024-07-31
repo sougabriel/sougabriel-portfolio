aff9c2db82fee1fefc1d1849aaff80692ba99ac7
sougabriel <gabriel.dsouzapro@gmail.com>
2024-07-31 12:21:31 -0300

    chore: remove unnecessary imports and elements

--- src/app/pages/projects/project-card/project-card.component.ts
+++ src/app/pages/projects/project-card/project-card.component.ts
@@ -1,34 +1,25 @@
-import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
-import { Project } from '../../../api';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { Project } from '@api/interfaces';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIcon } from '@angular/material/icon';
 import { NgOptimizedImage } from '@angular/common';
-import { MatDialog } from '@angular/material/dialog';
-import { ProjectDetails } from './project-details/project-details';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'project-card',
     standalone: true,
-    imports: [MatButtonModule, MatCardModule, MatIcon, NgOptimizedImage],
+    imports: [
+        MatButtonModule,
+        MatCardModule,
+        MatIcon,
+        NgOptimizedImage,
+        RouterLink,
+    ],
     templateUrl: './project-card.component.html',
     styleUrl: './project-card.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProjectCardComponent {
     project = input.required<Project>();
-
-    protected readonly dialog = inject(MatDialog);
-
-    protected openDialog(
-        enterAnimationDuration: string,
-        exitAnimationDuration: string,
-        project: Project
-    ): void {
-        this.dialog.open(ProjectDetails, {
-            enterAnimationDuration,
-            exitAnimationDuration,
-            data: project,
-        });
-    }
 }
