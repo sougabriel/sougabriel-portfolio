e09abee236bee65fb4958bb6d3751d6b0dc5039d
sougabriel <gabriel.dsouzapro@gmail.com>
2024-07-31 12:14:30 -0300

    chore: add markdown provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,24 +3,28 @@ import {
     provideZoneChangeDetection,
     isDevMode,
 } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideServiceWorker } from '@angular/service-worker';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
 import { LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { provideMarkdown } from 'ngx-markdown';
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideZoneChangeDetection({ eventCoalescing: true }),
         { provide: LocationStrategy, useClass: PathLocationStrategy },
-        provideRouter(routes),
+        provideRouter(routes, withComponentInputBinding()),
         provideAnimationsAsync(),
         provideHttpClient(withFetch()),
         provideServiceWorker('ngsw-worker.js', {
             enabled: !isDevMode(),
             registrationStrategy: 'registerWhenStable:30000',
+        }),
+        provideMarkdown({
+            loader: HttpClient
         }),
     ],
 };
