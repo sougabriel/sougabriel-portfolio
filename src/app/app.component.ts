6bf3734a96b8e1a71fa326e3f9a87d684b388f0e
sougabriel <gabriel.dsouzapro@gmail.com>
2024-08-08 20:13:27 -0300

    chore: remove cookie-popup component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CookiePopup } from '@shared/cookie-popup/cookie-popup';
+import { CookiePopup } from '@shared/services/cookie-popup/cookie.service';
 import { NavbarComponent } from '@shared/navbar/navbar.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [RouterOutlet, NavbarComponent, CookiePopup],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, NavbarComponent],
+    template: `
+        <navbar>
+            <router-outlet />
+        </navbar>
+    `,
+    styles: ``,
 })
-export class AppComponent {}
+export class AppComponent {
+    protected readonly cookieService = inject(CookiePopup);
+    constructor() {
+        this.cookieService.showSimpleMessage(
+            'This site does not uses cookies! Have a good time browsing.',
+            'Okay'
+        );
+    }
+}
