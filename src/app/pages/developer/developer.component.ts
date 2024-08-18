2135d41983a72782446bb9159b59cfbbd3f88d4e
sougabriel <gabriel.dsouzapro@gmail.com>
2024-08-18 12:35:07 -0300

    feat: create developer component

---
+++ src/app/pages/developer/developer.component.ts
@@ -0,0 +1,18 @@
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { DeveloperService } from '@api/services';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { DeveloperAvatar } from './avatar/avatar.component';
+import { DeveloperInformations } from './informations/informations.component';
+
+@Component({
+    selector: 'developer',
+    standalone: true,
+    imports: [DeveloperAvatar, DeveloperInformations],
+    templateUrl: './developer.component.html',
+    styleUrl: './developer.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DeveloperComponent {
+    protected readonly devService = inject(DeveloperService);
+    protected readonly developer = toSignal(this.devService.getDeveloper());
+}
