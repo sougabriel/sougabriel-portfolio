aee1bc27db17f765c75bd949588623958f741e2b
sougabriel <gabriel.dsouzapro@gmail.com>
2024-09-13 14:35:59 -0300

    chore: add certifications and education components imports

--- src/app/pages/developer/developer.component.ts
+++ src/app/pages/developer/developer.component.ts
@@ -3,11 +3,18 @@ import { DeveloperService } from '@api/services';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { DeveloperAvatar } from './avatar';
 import { DeveloperInformations } from './informations';
+import { CertificationsComponent } from './certifications';
+import { EducationComponent } from './education';
 
 @Component({
     selector: 'developer',
     standalone: true,
-    imports: [DeveloperAvatar, DeveloperInformations],
+    imports: [
+        DeveloperAvatar,
+        DeveloperInformations,
+        CertificationsComponent,
+        EducationComponent,
+    ],
     templateUrl: './developer.component.html',
     styleUrl: './developer.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
