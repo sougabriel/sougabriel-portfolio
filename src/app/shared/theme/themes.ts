1a95c59179b629562ab19baf2ba17c490dd94aee
sougabriel <gabriel.dsouzapro@gmail.com>
2024-09-12 21:22:21 -0300

    chore: add themes to a separete file

---
+++ src/app/shared/theme/themes.ts
@@ -0,0 +1,28 @@
+import { Theme } from "./theme-picker";
+
+export const THEMES: Theme[] = [
+    {
+        color: '#d7e3ff',
+        displayName: 'Azure & Blue',
+        name: 'azure-blue',
+        background: '#fdfbff'
+    },
+    {
+        color: '#ffd9e1',
+        displayName: 'Rose & Red',
+        name: 'rose-red',
+        background: '#fffbff'
+    },
+    {
+        color: '#00458f',
+        displayName: 'Dark Azure & Cyan',
+        name: 'azure-cyan',
+        background: '#191c1c'
+    },
+    {
+        color: '#810081',
+        displayName: 'Magenta & Violet',
+        name: 'magenta-violet',
+        background: '#1e1a1d'
+    },
+]
