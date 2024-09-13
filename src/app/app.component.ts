75e4a467808bd26bcedf14a731ddc34744dbc6af
sougabriel <gabriel.dsouzapro@gmail.com>
2024-09-13 14:49:31 -0300

    chore: text ident

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '@shared/navbar/navbar.component';
+
 @Component({
     selector: 'app-root',
     standalone: true,
