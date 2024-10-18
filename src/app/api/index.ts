4ef67b456b35b05e41a566768e172ef2f679242a
Gabriel de Souza <110489605+sougabriel@users.noreply.github.com>
2024-10-18 19:37:44 -0300

    chore(api): add api objects

--- src/app/api/index.ts
+++ src/app/api/index.ts
@@ -1,2 +1,6 @@
 export * from './services';
 export * from './interfaces';
+export { certifications } from './certifications';
+export { contacts } from './contacts';
+export { education } from './education';
+export { projects } from './projects';
